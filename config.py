ce4c65ccb44799ff420ca7708444afe6642feb4c
DheerajReddy04 <131962861+DheerajReddy04@users.noreply.github.com>
2025-02-22 04:46:46 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
-    ALLOWED_EXTENSIONS = {'pdf', 'docx'}
